44d1a58dd43668ed1fee5ce52ff5a1761b706e18
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2024-05-27 15:01:43 +0530

    Update ContactLeft.js

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -28,17 +28,17 @@ const ContactLeft = () => {
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-        <a href="https://instagram.com/_adityapathakk" target="_blank">
+        <a href="https://instagram.com/_adityapathakk" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaInstagram />
               </span>
             </a>
-            <a href="https://github.com/pathakadi" target="_blank">
+            <a href="https://github.com/pathakadi" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaGithub />
               </span>
             </a>
-            <a href="https://www.linkedin.com/in/aditya-pathak-330703292/" target="_blank">
+            <a href="https://www.linkedin.com/in/aditya-pathak-330703292/" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaLinkedinIn />
               </span>
